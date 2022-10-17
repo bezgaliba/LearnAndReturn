6c820b8ccf4b66513f2ced39d1cde233d9879a13
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2022-10-17 12:12:23 +0300

    Provided CF Deployment config

---
+++ app/app_worklist/dist/controller/App.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["./BaseController"],function(t){"use strict";return t.extend("appworklist.controller.App",{onInit:function(){this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())}})});
---
+++ app/app_worklist/dist/controller/NotFound.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["./BaseController"],function(t){"use strict";return t.extend("appworklist.controller.NotFound",{onLinkPressed:function(){this.getRouter().navTo("worklist")}})});
