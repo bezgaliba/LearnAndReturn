a79194374026ed09d74052ee47579fb0e0b542d3
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2022-10-24 10:58:30 +0300

    code clean

--- app/learn_and_return/webapp/controller/ErrorHandler.js
+++ app/learn_and_return/webapp/controller/ErrorHandler.js
@@ -3,32 +3,23 @@ sap.ui.define([
     "sap/m/MessageBox",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator"
-], function (UI5Object, MessageBox, Filter, FilterOperator) {
+], function(UI5Object, MessageBox, Filter, FilterOperator) {
     "use strict";
 
     return UI5Object.extend("learnandreturn.controller.ErrorHandler", {
-
-        /**
-         * Handles application errors by automatically attaching to the model events and displaying errors when needed.
-         * @class
-         * @param {sap.ui.core.UIComponent} oComponent reference to the app's component
-         * @public
-         * @alias learnandreturn.controller.ErrorHandler
-         */
-        constructor : function (oComponent) {
+        constructor: function(oComponent) {
             var oMessageManager = sap.ui.getCore().getMessageManager(),
                 oMessageModel = oMessageManager.getMessageModel(),
                 oResourceBundle = oComponent.getModel("i18n").getResourceBundle(),
-                sErrorText = oResourceBundle.getText("errorText"),
-                sMultipleErrors = oResourceBundle.getText("multipleErrorsText");
+                sErrorText = oResourceBundle.getText("objectPatternError"),
+                sMultipleErrors = oResourceBundle.getText("fatalError");
 
             this._oComponent = oComponent;
             this._bMessageOpen = false;
 
-            this.oMessageModelBinding = oMessageModel.bindList("/", undefined,
-                [], new Filter("technical", FilterOperator.EQ, true));
+            this.oMessageModelBinding = oMessageModel.bindList("/", undefined, [], new Filter("technical", FilterOperator.EQ, true));
 
-            this.oMessageModelBinding.attachChange(function (oEvent) {
+            this.oMessageModelBinding.attachChange(function(oEvent) {
                 var aContexts = oEvent.getSource().getContexts(),
                     aMessages = [],
                     sErrorTitle;
@@ -38,7 +29,7 @@ sap.ui.define([
                 }
 
                 // Extract and remove the technical messages
-                aContexts.forEach(function (oContext) {
+                aContexts.forEach(function(oContext) {
                     aMessages.push(oContext.getObject());
                 });
                 oMessageManager.removeMessages(aMessages);
@@ -50,24 +41,15 @@ sap.ui.define([
                 this._showServiceError(sErrorTitle, aMessages[0].message);
             }, this);
         },
-
-        /**
-         * Shows a {@link sap.m.MessageBox} when a service call has failed.
-         * Only the first error message will be displayed.
-         * @param {string} sErrorTitle A title for the error message
-         * @param {string} sDetails A technical error to be displayed on request
-         * @private
-         */
-        _showServiceError : function (sErrorTitle, sDetails) {
+        _showServiceError: function(sErrorTitle, sDetails) {
             this._bMessageOpen = true;
             MessageBox.error(
-                sErrorTitle,
-                {
-                    id : "serviceErrorMessageBox",
+                sErrorTitle, {
+                    id: "serviceErrorMessageBox",
                     details: sDetails,
                     styleClass: this._oComponent.getContentDensityClass(),
                     actions: [MessageBox.Action.CLOSE],
-                    onClose: function () {
+                    onClose: function() {
                         this._bMessageOpen = false;
                     }.bind(this)
                 }
