93a6add7c5dd9a965ca03d92ab1a8595e0e9fe25
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2022-10-28 10:25:00 +0300

    Course mock up

---
+++ app/learn_and_return/webapp/controller/Course.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "./BaseController"
+], function(BaseController) {
+    "use strict";
+
+    return BaseController.extend("learnandreturn.controller.Course", {
+
+        onInit: function() {}
+    });
+
+});
