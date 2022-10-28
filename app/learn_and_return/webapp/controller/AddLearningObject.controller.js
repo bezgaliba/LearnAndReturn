95eaf1e6fc43e4a1a1dd37cac53396c327b4f09d
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2022-10-28 17:18:38 +0300

    Initial commit learning object creation page

---
+++ app/learn_and_return/webapp/controller/AddLearningObject.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "./BaseController"
+], function(BaseController) {
+    "use strict";
+
+    return BaseController.extend("learnandreturn.controller.AddLearningObject", {
+
+        onInit: function() {}
+    });
+
+});
