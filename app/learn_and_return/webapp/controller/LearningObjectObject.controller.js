a2d9d93fe75bdd73f26f84c6a53ac3d04a00352c
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2022-10-28 17:15:23 +0300

    Implement learning object UI

--- app/learn_and_return/webapp/controller/LearningObjectObject.controller.js
+++ app/learn_and_return/webapp/controller/LearningObjectObject.controller.js
@@ -43,6 +43,10 @@ sap.ui.define([
                 this.getRouter().getTargets().display("objectNotFound");
                 return;
             }
-        }
+        },
+        onNavLOList: function() {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("learningObjectList", {});
+        },
     });
 });
