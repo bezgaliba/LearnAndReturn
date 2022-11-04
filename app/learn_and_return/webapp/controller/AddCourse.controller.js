d43c866b3c6a04f506e9bd55fefc2a35e4746625
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2022-11-04 10:19:40 +0200

    UI creation of add course page

--- app/learn_and_return/webapp/controller/AddCourse.controller.js
+++ app/learn_and_return/webapp/controller/AddCourse.controller.js
@@ -1,9 +1,39 @@
 sap.ui.define([
-    "./BaseController"
+    "./BaseController",
 ], function(BaseController) {
     "use strict";
 
     return BaseController.extend("learnandreturn.controller.AddCourse", {
         onInit: function() {},
+        onNavLOList: function() {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("learningObjectList", {}, true);
+        },
+        onCreate: function() {
+            let oListBinding = this.getView().getModel().bindList('/LearningObject'),
+                oObjectNameField = this.getView().byId("formLearningObjectName"),
+                oType_IDField = this.getView().byId("formLearningObjectType"),
+                oContentField = this.getView().byId("formLearningObjectContent"),
+                oDescriptionField = this.getView().byId("formLearningObjectDescription");
+            oListBinding.create({
+                Name: oObjectNameField.getValue(),
+                Type_ID: oType_IDField.getSelectedKey(),
+                Content: oContentField.getValue(),
+                Description: oDescriptionField.getValue()
+            });
+            var self = this;
+            this.onNavLOList();
+            this.clearFields();
+        },
+        clearFields: function() {
+            let oObjectNameField = this.getView().byId("formLearningObjectName"),
+                oType_IDField = this.getView().byId("formLearningObjectType"),
+                oContentField = this.getView().byId("formLearningObjectContent"),
+                oDescriptionField = this.getView().byId("formLearningObjectDescription");
+            oObjectNameField.setValue("");
+            oType_IDField.setValue("");
+            oContentField.setValue("");
+            oDescriptionField.setValue("");
+        }
     });
 });
