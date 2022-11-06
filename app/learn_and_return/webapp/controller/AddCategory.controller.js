1e04bbed689e6449dc027991677501b7bf98b3a3
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2022-11-06 19:41:09 +0200

    Prepare category creation UI

---
+++ app/learn_and_return/webapp/controller/AddCategory.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "./BaseController"
+], function(BaseController) {
+    "use strict";
+
+    return BaseController.extend("learnandreturn.controller.AddCategory", {
+
+        onInit: function() {}
+    });
+});
